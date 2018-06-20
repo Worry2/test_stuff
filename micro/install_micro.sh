510d559017245a81b6bce3fa3981b62c7cdf2061
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2018-06-20 23:00:07 +0300

    micro example

---
+++ micro/install_micro.sh
@@ -0,0 +1,14 @@
+go get -u github.com/micro/micro
+go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+go get -u github.com/micro/protoc-gen-micro
+
+# Set $GOPATH to be able to run micro new
+
+# Install protobuf: get source and compile:
+# https://github.com/google/protobuf
+# ./configure
+# ./autogen.sh
+# make
+# make install
+
+# NOTES: mdns registry does not work well...
