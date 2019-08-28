629c65412ae1fc77615761a30ed2189e57474d78
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2019-08-28 11:46:12 +0300

    Verify signature

--- softhsm/main.go
+++ softhsm/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"crypto"
 	"encoding/base64"
+	"encoding/pem"
 	"fmt"
+	"io/ioutil"
 
 	"crypto/rand"
 	"crypto/rsa"
 	_ "crypto/sha256"
+	"crypto/x509"
 
 	"github.com/ThalesIgnite/crypto11"
 )
@@ -38,19 +41,37 @@ func main() {
 	if d, ok := s.(crypto.Decrypter); ok {
 		fmt.Println("Public key for keypair:")
 		fmt.Println(d.Public().(*rsa.PublicKey).N)
+
+		asn1, err := x509.MarshalPKIXPublicKey(d.Public())
+		if err != nil {
+			panic(err)
+		}
+
+		pubBytes := pem.EncodeToMemory(&pem.Block{
+			Type:  "PUBLIC KEY",
+			Bytes: asn1,
+		})
+
+		fmt.Println("Public key PEM (written in key.pub):")
+		fmt.Println(string(pubBytes))
+
+		ioutil.WriteFile("key.pub", pubBytes, 0644)
 	}
 
-	plaintext := []byte("sign me with RSA")
+	tbs := []byte("sign me with RSA")
+
 	h := crypto.SHA256.New()
-	h.Write(plaintext)
+	h.Write(tbs)
 	hash := h.Sum([]byte{})
+	ioutil.WriteFile("tbs.txt", tbs, 0644)
 
 	sig, err := s.Sign(rand.Reader, hash, crypto.SHA256)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to sign: %v", err))
 	}
 
-	fmt.Println("Signature: ", base64.StdEncoding.EncodeToString(sig))
+	fmt.Println("Base64 encoded signature: ", base64.StdEncoding.EncodeToString(sig))
+	ioutil.WriteFile("sig.bin", sig, 0644)
 
 	// TODO: Verify
 	fmt.Println("The End")
