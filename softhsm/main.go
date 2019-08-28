73805d63a74261aa1fa7c7958618d380f6825f32
Pasi Tähkäpää <pasi.m.t.tahkapaa@gmail.com>
2019-08-28 11:23:36 +0300

    Generate and load key, fetch public key

--- softhsm/main.go
+++ softhsm/main.go
@@ -1,50 +1,56 @@
 package main
 
 import (
+	"crypto"
+	"encoding/base64"
 	"fmt"
 
+	"crypto/rand"
+	"crypto/rsa"
 	_ "crypto/sha256"
 
 	"github.com/ThalesIgnite/crypto11"
 )
 
 func main() {
-	_, err := crypto11.ConfigureFromFile("config.softhsm2")
+	ctx, err := crypto11.ConfigureFromFile("config.softhsm2")
 	if err != nil {
 		panic(err)
 	}
+	defer ctx.Close()
 
-	// pvk, err := crypto11.FindKeyPairOnSlot(0, nil, []byte("TestToken"))
-	// if err != nil {
-	// 	panic(err)
-	// }
+	id := []byte("id")
+	label := []byte("label")
 
-	// signingKey, ok := pvk.(*crypto11.PKCS11PrivateKeyRSA)
-	// if !ok {
-	// 	panic("Failed to type assert to *crypto11.PKCS11PrivateKeyRSA")
-	// }
+	s, err := ctx.FindKeyPair(id, label)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	// id, label, err := signingKey.Identify()
-	// if err != nil {
-	// 	panic(fmt.Sprintf("Failed to identify key: %v", err))
-	// }
-	// fmt.Printf("Key Label: '%s', ID: '%d'\n", label, id)
+	if s == nil {
+		sd, err := ctx.GenerateRSAKeyPairWithLabel(id, label, 2048)
+		if err != nil {
+			panic(err)
+		}
+		s = sd
+	}
 
-	// if err := signingKey.Validate(); err != nil {
-	// 	panic(fmt.Sprintf("Failed to validate key: %v", err))
-	// }
+	if d, ok := s.(crypto.Decrypter); ok {
+		fmt.Println("Public key for keypair:")
+		fmt.Println(d.Public().(*rsa.PublicKey).N)
+	}
 
-	// plaintext := []byte("sign me with RSA")
-	// h := crypto.SHA256.New()
-	// h.Write(plaintext)
-	// hash := h.Sum([]byte{})
+	plaintext := []byte("sign me with RSA")
+	h := crypto.SHA256.New()
+	h.Write(plaintext)
+	hash := h.Sum([]byte{})
 
-	// sig, err := signingKey.Sign(rand.Reader, hash, crypto.SHA256)
-	// if err != nil {
-	// 	panic(fmt.Sprintf("Failed to sign: %v", err))
-	// }
+	sig, err := s.Sign(rand.Reader, hash, crypto.SHA256)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to sign: %v", err))
+	}
 
-	// fmt.Println("Signature: ", base64.StdEncoding.EncodeToString(sig))
+	fmt.Println("Signature: ", base64.StdEncoding.EncodeToString(sig))
 
 	// TODO: Verify
 	fmt.Println("The End")
